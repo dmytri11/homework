5a6618d4c853a88b284648119d8a450c94f86012
dima_baranov <azyscvant@gmail.com>
2023-08-11 18:28:53 +0300

    Второе дз

---
+++ dz2t1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int Prompt(string message)
+{
+
+    System.Console.Write(message);
+    string value = Console.ReadLine ();
+    int resualt =Convert.ToInt32(value);
+    return resualt;
+
+}
+int number = Prompt("Введите трёхзначное число > ");
+if (number < 100 || number>= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число, введите другое значнеие)");
+     return;
+}
+Console.WriteLine($"Введённое число `{number}`");
+int SecondRank= number  / 10 % 10;
+Console.WriteLine($"Вторая цифра `{SecondRank}`" );
---
+++ dz2t2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int Prompt(string message)
+{
+
+    System.Console.Write(message);
+    string value = Console.ReadLine ();
+    int resualt =Convert.ToInt32(value);
+    return resualt;
+
+}
+int GetThirdRank(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+bool ValidateNumber(int number)
+{
+if (number < 100)
+{
+    Console.WriteLine(«Третьей цифры нет»);
+    return false;
+}
+ return true;
+}
+int number = Prompt("Введите число > ");
+ int (ValidateNumber(number))
+ {
+    Console.WriteLine(GetThirdRank(number));
+ }
+
