17881afc5146b84bafcdfc6269474aed5ec3d560
dima_baranov <azyscvant@gmail.com>
2023-09-03 22:49:14 +0300

    4 дз

---
+++ dz4task2(36)/Program.cs
@@ -0,0 +1,35 @@
+﻿ //Задайте одномерный массив, заполненный случайными числами. 
+ //Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+//Задайте одномерный массив, заполненный случайными числами. 
+ //Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+Console.WriteLine("Задайте размер массива");
+int massiv =Convert.ToInt32(Console.ReadLine());
+int[] array = new int [massiv];
+int sum = 0;
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    Console.WriteLine(String.Join(" ", array));
+   return array;
+}
+int[] newarray = FillArray(array);
+int SumElements(int[] newarray, int sum)
+{
+
+      for (int i = 1; i < newarray.Length; i +=2)
+{
+        sum = sum + newarray[i];
+}
+return sum;
+}
+
+int sum2=SumElements(newarray,sum);
+
+Console.WriteLine($"Сумма элементов стоящих на нечётных позиция массива {sum2}");
---
+++ dz4task3(38)/Program.cs
@@ -0,0 +1,12 @@
+﻿//: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+double[] array = new double[8];
+Random rand =  new Random();
+for (int i = 0; i < array.Length; i++)
+array[i] = Math.Round((rand.Next(-100, 100) + rand.NextDouble()), 2);
+double max = array.Max();
+double min = array.Min();
+double raznica = max - min;
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"Разница между максимальным и минимальным элементои {raznica}");
