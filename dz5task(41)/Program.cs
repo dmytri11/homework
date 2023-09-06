bf0b0f2ae4994e4d162d96efe30bc36545789687
dima_baranov <azyscvant@gmail.com>
2023-09-06 21:28:48 +0300

    5 дз

---
+++ dz5task(41)/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите М чисел через запятую");
+string valueStr = Console.ReadLine();
+string[] values = valueStr.Split(",");
+Console.Write("[");
+Console.WriteLine(String.Join(", ", values));
+Console.Write("]");
+int result = 0;
+void proverka(string[] values,int result)
+{   
+     for (int i = 0; i < values.Length; i++)
+     {
+          int a = Convert.ToInt32(values[i]);
+          if (a > 0 )
+          {
+            result++;
+          }
+     }
+      Console.WriteLine($"Количество четных элементов {result}");
+}
+ proverka(values, result);
---
+++ dz5task(43)/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Введите четыре значения");
+int b1 = Convert.ToInt32(Console.ReadLine());
+int k1 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+ y1,y2;
