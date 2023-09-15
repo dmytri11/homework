563010ac57038eeb0cdb261490fc73e37eb2e59a
dima_baranov <azyscvant@gmail.com>
2023-09-15 18:32:52 +0300

    8 дз

---
+++ dz8task58/Program.cs
@@ -0,0 +1,70 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+int[,] AddArray()
+{
+    Console.WriteLine("задайте параметры матрицы: ");
+    int lengthArray = int.Parse(Console.ReadLine());
+    int getArray = int.Parse(Console.ReadLine());
+    int[,] array = new int[lengthArray,getArray];
+    return array;
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5);
+
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void proizvod(int[,] array, int[,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j] * array2[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] Multiplication(int[,] array, int[,] array2)
+{
+    if (array.GetLength(1) != array2.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+    int[,] r = new int[array.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2.GetLength(0); k++)
+            {
+                r[i, j] += array[i, k] * array2[k, j];
+
+            }
+            Console.Write(r[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return r;
+}
+int[,] array = AddArray();
+int[,] array2 = AddArray();
+Console.WriteLine("--------");
+FillArray(array);
+Console.WriteLine("--------");
+FillArray(array2);
+Console.WriteLine("--------");
+Console.WriteLine("Умножение элементов по индексам");
+proizvod(array, array2);
+Console.WriteLine("---------------");
+Console.WriteLine("Умножение элементов по методу матриц(строка на столбик)");
+Multiplication(array, array2);
---
+++ dz8task59/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец,
+//на пересечении которых расположен наименьший элемент массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Наименьший элемент - 1, на выходе получим
+//следующий массив:
+//9 2 3
+//4 2 4
+//2 6 7
+
+int[,] AddArray()
+{
+    Console.WriteLine("задайте параметры матрицы: ");
+    int lengthArray = int.Parse(Console.ReadLine());
+    int getArray = int.Parse(Console.ReadLine());
+    int[,] array = new int[lengthArray,getArray];
+    return array;
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5);
+
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void MinElements(int[,] array,int MinEl,int minindexi,int minindexj)
+{
+    
+    for (int i = 0; i <array.GetLength(0); i++)
+        {
+            for (int j = 0; j <array.GetLength(1) ; j++)
+            {
+                if (array[i, j] <= MinEl)
+                    {
+                        
+                        minindexi = i;
+                        minindexj = j;
+                        MinEl = array[i, j];
+                    }
+            }
+        }
+       ;
+}
+////всё сломалось на этой функции всё сломалось времени нет буду завтра разбираться
+///
+
+int[,] array = AddArray();
+
+Console.WriteLine("--------");
+FillArray(array);
+int minindexi = 0;
+int minindexj = 0;
+int MinEl = array[0, 0];
+Console.WriteLine("--------");
+
+Console.WriteLine($"{MinEl},{minindexi},{minindexj}");
