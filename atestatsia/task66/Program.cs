7266629639976c727fc4c70115d65c486640f0e9
dima_baranov <azyscvant@gmail.com>
2023-09-17 21:31:34 +0300

    Атестация

---
+++ atestatsia/task66/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int SumNumber (int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    return N + SumNumber(M, N - 1);
+}
+
+
+Console.WriteLine(SumNumber(M, N));
+
+
+
---
+++ atestatsia/task68/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите два неотрицательных числа m и n:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Ackermann(m, n);
+
+Console.WriteLine($"Результат функции Аккермана для m={m} и n={n} равен {result}");
+
+static int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
